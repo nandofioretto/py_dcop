7576cbed91fac3746675446bdfa3d9efb8a3dbf0
fioretto@umich.edu <fioretto@umich.edu>
2018-08-27 09:39:36 -0400

    scripts

---
+++ src/scripts/run_rand_dense.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+algorithms=("dsa" "maxsum" "ccg-maxsum" "ccg-maxsum-c" "ccg-dsa"  "dsa&ccg-maxsum" "dsa&ccg-maxsum-c" "dsa&ccg-dsa"  "dsa&rand")
+py_dcop_home="/home/fioretto/Repos/py_dcop"
+#py_dcop_home="/Users/nando/Repos/DCOP/py_dcop"
+cd ${py_dcop_home}
+
+seeds=(10 20 30 40 50 60 70 80 90 100)
+for seed in ${seeds[@]}; do
+	dcop_file="${py_dcop_home}/data/in/rand-dense_${seed}.json"
+	python src/py_dcop2.py --nagents=100 --domsize=3 --seed=${seed} --graph="rand-dense" --fileout="${dcop_file}"
+	
+	for alg in ${algorithms[@]}; do
+		echo "rand dense " $alg " seed " $seed
+		python src/py_dcop2.py --algorithm="${alg}" \
+	 	       --iterations=5000 \
+	 	       --seed=${seed} \
+	 	       --filein="${dcop_file}" \
+		       --fileout="${py_dcop_home}/data/out/${alg}_rand_dense_s${seed}_.csv"
+	   done
+done
